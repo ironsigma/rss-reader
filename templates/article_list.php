b41cfb7363aca249ad259e0213e47947ef944940
Juan D Frias <jfrias@boxfi.com>
2013-03-07 13:45:49 -0800

    Added new layout and less/css

--- templates/article_list.php
+++ templates/article_list.php
@@ -1,24 +1,38 @@
 <?php $page_title = ' - '. $feed_name .' ('. $article_count .')' ?>
-<?php $page_css = array('article.css') ?>
-<?php $page_js = array('jquery.js') ?>
+<?php $page_css = array('article_list.less') ?>
+<?php $page_js = array('jquery.js', 'less.js') ?>
 <?php include 'header_block.php' ?>
 
-<a href="/">Back To Feed List</a>
-<h1><?php echo $feed_name ?> (<?php echo $article_count ?>)</h1>
-<?php if ( $article_count === 0 ) : ?>
-<p>No unread items.</p>
-<?php else : ?>
-<?php include 'article_pager.php' ?>
-<?php foreach($articles as $a) : ?>
-<div class='article'>
-    <div id="star_<?php echo $a->stared?'1':'0' ?>_<?php echo $a->id ?>" class='star'><img id="star_img_<?php echo $a->id ?>" src="/static/images/star_<?php echo $a->stared?'full':'empty' ?>.png"/></div>
-    <h2><a href="<?php echo $a->link ?>"><?php echo $a->title ?></a></h2>
-    <h3><?php echo date('D M j, Y &\m\d\a\s\h; g:i a', $a->published) ?></h3>
-    <div class='text'><?php echo $a->text ?></div>
-</div>
-<?php endforeach ?>
-<?php include 'article_pager.php' ?>
-<?php endif ?>
+<div id="container">
+    <div id="header">
+        <h1><?php echo $feed_name ?> (<?php echo $article_count ?>)</h1>
+    </div><!-- header -->
+    <div id="navigation">
+        <ul>
+            <li><a href="/">Feeds</a></li>
+        </ul>
+    </div><!-- navigation -->
+    <div id="content">
+        <?php if ( $article_count === 0 ) : ?>
+        <p>No unread items.</p>
+        <?php else : ?>
+        <?php include 'article_pager.php' ?>
+        <?php foreach($articles as $a) : ?>
+        <div class='article'>
+            <div id="star_<?php echo $a->stared?'1':'0' ?>_<?php echo $a->id ?>" class='star'><img id="star_img_<?php echo $a->id ?>" src="/static/images/star_<?php echo $a->stared?'full':'empty' ?>.png"/></div>
+            <h2><a href="<?php echo $a->link ?>"><?php echo $a->title ?></a></h2>
+            <h3><?php echo date('D M j, Y &\m\d\a\s\h; g:i a', $a->published) ?></h3>
+            <div class='text'><?php echo $a->text ?></div>
+        </div>
+        <?php endforeach ?>
+        <?php include 'article_pager.php' ?>
+        <?php endif ?>
+    </div><!-- content -->
+    <div id="footer">
+        &copy; 2013 AxiSym3.net All Right Reserved
+    </div><!-- footer -->
+</div><!-- container -->
+
 <script type="text/javascript">
 $(document).ready(function(){
     $(".star").click(function(event) {
@@ -43,6 +57,5 @@ $(document).ready(function(){
     });
 });
 </script>
-<div id="footer">&copy; 2013 AxiSym3.net All Right Reserved</div>
 
 <?php include 'footer_block.php' ?>
--- templates/header_block.php
+++ templates/header_block.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <link rel="icon" type="image/png" href="/static/images/favicon.ico">
     <?php if ( isset($page_css) ) foreach ( $page_css as $style ) : ?>
-    <link rel="stylesheet" href="/static/css/<?php echo $style ?>">
+    <link type="text/css" rel="stylesheet<?php echo (substr_compare($style, '.less', -5, 5)===0?'/less':'') ?>" href="/static/css/<?php echo $style ?>">
     <?php endforeach ?>
     <?php if ( isset($page_js) ) foreach ( $page_js as $script ) : ?>
     <script type="text/javascript" src="/static/js/<?php echo $script ?>"></script>
