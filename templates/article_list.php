46b5e216c899387f312d2d75171ed80abb9f7fd1
Juan D Frias <jfrias@boxfi.com>
2014-09-03 12:17:35 -0700

    Added rel next and prev to paging links

--- templates/article_list.php
+++ templates/article_list.php
@@ -40,11 +40,11 @@
             <a href="/feed/<?php echo $feed_id ?>/read?<?php echo $container ?>=<?php echo $feed_id ?>&amp;page=1&amp;ids=all">&times;&times;Mark All Read</a>
         <?php endif ?>
         <?php if ($page > 1) : ?>
-            <a style="margin-left:10px" href="/<?php echo $container ?>/<?php echo $feed_id ?>/articles?page=<?php echo $page - 1 ?>">&laquo; Prev</a>
+            <a style="margin-left:10px" rel="prev" href="/<?php echo $container ?>/<?php echo $feed_id ?>/articles?page=<?php echo $page - 1 ?>">&laquo; Prev</a>
         <?php endif ?>
         <span style="margin-left:10px">Page <?php echo $page ?> of <?php echo $page_count ?></span>
         <?php if ($page < $page_count) : ?>
-            <a style="margin-left:10px" href="/<?php echo $container ?>/<?php echo $feed_id ?>/articles?page=<?php echo $page + 1 ?>">Next &raquo;</a>
+            <a style="margin-left:10px" rel="next" href="/<?php echo $container ?>/<?php echo $feed_id ?>/articles?page=<?php echo $page + 1 ?>">Next &raquo;</a>
         <?php endif ?>
         <?php if ($article_count > 0 && $feed_id !== 'stared') : ?>
             <a style="margin-left:10px" href="/feed/<?php echo $feed_id ?>/read?<?php echo $container ?>=<?php echo $feed_id ?>&amp;page=<?php echo $page ?>&amp;ids=<?php echo $article_ids ?>">&times; Mark Page Read</a>
